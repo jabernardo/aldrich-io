ecde76cce5b64a73f09e1d5b2365f6db0c8c06a6
John Aldrich Bernardo <4ldrich@protonmail.com>
2019-12-30 23:43:03 +0800

    Updates

---
+++ precache-manifest.a6ad780ebdeba9e31fde4c55444eed59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bcc64d086992f96bd5c0e3e3b49364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac30a50e07474755c73",
+    "url": "/static/css/2.b76d4783.chunk.css"
+  },
+  {
+    "revision": "b2d60e3174152deaf8e1",
+    "url": "/static/css/main.b923971e.chunk.css"
+  },
+  {
+    "revision": "7ac30a50e07474755c73",
+    "url": "/static/js/2.7bde0b15.chunk.js"
+  },
+  {
+    "revision": "b2d60e3174152deaf8e1",
+    "url": "/static/js/main.68a19181.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
