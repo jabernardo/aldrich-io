bc9fce9fdabd0ab136e40dbd0cf6eec996edd5b8
John Aldrich Bernardo <4ldrich@protonmail.com>
2021-03-22 20:18:29 +0800

    fix: use cloudinary and cls fix

--- widgets/Header.js
+++ widgets/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Image, Placeholder } from "cloudinary-react";
 
 import * as Skeleton from "../components/Skeleton";
 
@@ -9,7 +10,20 @@ class Header extends Component {
     return (
       <Skeleton.Box className={styles.header}>
         <section>
-          <img className={styles.profile} src="/assets/me-prof.png" alt={this.props.title || "John Aldrich Bernardo"} />
+          <Image
+            publicId="portfolio/me-prof_ghmwhg.png"
+            height={256}
+            width={256}
+            loading="lazy"
+            dpr="auto"
+            responsive
+            crop="scale"
+            responsiveUseBreakpoints="true"
+            className={styles.profile}
+          >
+            <Placeholder type="pixelate" />
+          </Image>
+
           <h1>{this.props.title || "John Aldrich Bernardo"}</h1>
           <a className={styles["cli-version"]} href="/cv-cli">&gt; View this page in CLI version</a>
         </section>
